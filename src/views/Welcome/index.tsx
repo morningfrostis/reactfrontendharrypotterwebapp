1585e0d0c1f61118461637af0b9e7b928b95d7a8
Antonio <antoniodelaiglesia@gmail.com>
2023-02-25 14:32:36 +0100

    Feat: create originals colors for theme Harry Potter

--- src/views/Welcome/index.tsx
+++ src/views/Welcome/index.tsx
@@ -1,24 +1,25 @@
-import React, { FC, useCallback } from 'react'
+import React, { FC } from "react";
 import {
-    ButtonBoxes,
+  ButtonBoxes,
   RedirectMessage,
   WelcomeButton,
   WelcomeContainer,
   WelcomeMessage,
-} from './styles'
+} from "./styles";
 
 const Welcome: FC = () => {
-    
   return (
     <WelcomeContainer>
       <WelcomeMessage>Welcome to Harry Potter Web App</WelcomeMessage>
-      <RedirectMessage>Please login or create an account to continue</RedirectMessage>
-        <ButtonBoxes>
-      <WelcomeButton to="/login">Login</WelcomeButton>
-      <WelcomeButton to="/signup">Signup</WelcomeButton>
+      <RedirectMessage>
+        Please login or create an account to continue
+      </RedirectMessage>
+      <ButtonBoxes>
+        <WelcomeButton to="/login">Login</WelcomeButton>
+        <WelcomeButton to="/signup">Signup</WelcomeButton>
       </ButtonBoxes>
     </WelcomeContainer>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
