dd747eacedf97c0f03fe156bca2e50ed4167016a
ManuelRod <manuvski@gmail.com>
2023-02-25 14:09:00 +0100

    feat:introducing architecture project

---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+const theme = {
+    colors: {
+      accent: '#0074CA',
+      danger: '#E53935',
+      dangerPale: '#ffecec',
+      orange: '#FB762E',
+      orangePale: '#fff4ec',
+      green: '#2CB532',
+      greenPale: '#edf9ee',
+      black: '#000000',
+      blue100: '#83b0e7',
+      blue300: '#5393de',
+      blue700: '#2F73D8',
+      blue900: '#2C3E50',
+      grey100: '#FAFAFA',
+      grey200: '#E1E1E5',
+      grey300: '#BCC6CC',
+      grey400: '#A3A9B5',
+      grey500: '#819098',
+      grey700: '#9E9E9E',
+      grey900: '#6E7A76',
+      white100: '#FFFFFF',
+      white200: '#F5F5F5',
+      white300: '#E5E5E5',
+      white400: '#D4D4D4',
+      white500: '#C4C4C4',
+      white700: '#BCC6CC',
+      red100: '#E53935',
+      pink100: '#f72faa',
+      ReactBlue:'#282c34'
+    },
+  }
+
+  export default theme
---
+++ src/views/Welcome/index.tsx
@@ -0,0 +1,24 @@
+import React, { FC, useCallback } from 'react'
+import {
+    ButtonBoxes,
+  RedirectMessage,
+  WelcomeButton,
+  WelcomeContainer,
+  WelcomeMessage,
+} from './styles'
+
+const Welcome: FC = () => {
+    
+  return (
+    <WelcomeContainer>
+      <WelcomeMessage>Welcome to Harry Potter Web App</WelcomeMessage>
+      <RedirectMessage>Please login or create an account to continue</RedirectMessage>
+        <ButtonBoxes>
+      <WelcomeButton to="/login">Login</WelcomeButton>
+      <WelcomeButton to="/signup">Signup</WelcomeButton>
+      </ButtonBoxes>
+    </WelcomeContainer>
+  )
+}
+
+export default Welcome
