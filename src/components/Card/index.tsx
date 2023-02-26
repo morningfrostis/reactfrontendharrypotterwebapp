cef8a8357f3004e8c0bfba49df070cd03d6d00de
ManuelRod <manuvski@gmail.com>
2023-02-26 11:21:35 +0100

    feat:introduction of details view

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,25 +1,37 @@
-import { FC } from 'react'
-import { Link } from 'react-router-dom'
-import { Container, Description, Image } from './styles'
-import {Props} from './types'
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import { Container, Description, Image } from "./styles";
+import { Props } from "./types";
 
-const Card:FC<Props> = ({
-    name,
-    house,
-    image,
-    id
+const Card: FC<Props> = ({
+  name,
+  house,
+  image,
+  id,
+  species,
+  wizard,
+  ancestry,
+  wand,
+  patronus,
+  actor,
+  type = 'list', 
+
 }) => {
+  return (
+    <Container>
+      <Image src={image} />
+      {type === 'list' &&  <Description>Name: {name}</Description>}
+      {type === 'list' && <Description>Hogwarts house: {house}</Description>}
+      {type === 'details' &&  <Description>Specie: {species}</Description>}
+     {type === 'details' && <Description>Is a wizard? {wizard}</Description>}
+     {type === 'details' && <Description>Ancestry: {ancestry}</Description>}
+     {type === 'details' && <Description>Wand: {wand}</Description>}
+     {type === 'details' && <Description>Patronus: {patronus}</Description>}
+     {type === 'details' && <Description>Actor: {actor}</Description>}
+       {type === 'list' && <Link to={`/details/${id}`}>View detalles</Link>}
+      {type === 'details' && <Description>Id: {id}</Description>}
+    </Container>
+  );
+};
 
-    return(
-        <Container>
-            <Image src={image}/>
-            <Description> {name}</Description>
-            <Description> {house}</Description>
-            <Link to={`/details/${id}`}>View detalles</Link>
-            <Description> {id}</Description>
-
-        </Container>
-    )
-}
-
-export default Card
+export default Card;
