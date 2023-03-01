0be43251a0efd418753b9c91f81fa69d190693d3
Antonio <antoniodelaiglesia@gmail.com>
2023-03-01 11:18:26 +0100

    Feat: border container & styles-componets cards

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,25 +1,58 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+// export const Container = styled.div`
+//   border: 1px solid white;
+//   padding: 15px;
+//   width: 10em;
+// `;
+
 export const Container = styled.div`
-  border: 1px solid white;
-  padding: 15px;
-  width: 10em;
+  background: linear-gradient(315deg, #9d523c 0%, #f2a65a 74%);
+  flex: 0 0 300px;
+  padding: 5px;
+  padding-bottom: 25px;
+  margin: 2em;
+  border-radius: 20px;
+  box-shadow: 0 0 0 5px #740001;
+  text-align: center;
+  position: relative;
+  &:before {
+    content: "Harry Potter character";
+    font-family: "Oswald";
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 16px;
+    letter-spacing: 0.5px;
+    border-radius: 20px;
+    background: linear-gradient(to top, #9d523c 0%, #f2a65a 74%);
+    position: absolute;
+    z-index: 10;
+    max-width: 225px;
+    top: -15px;
+    padding: 5px;
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
 `;
 
 export const Image = styled.img`
   width: 150px;
   height: 200px;
+  margin-top: 40px;
   margin-bottom: 5px;
+  border-radius: 6px;
 `;
 
 export const Description = styled.p`
   margin-right: 1px;
   font-size: 18px;
-  margin-bottom: 10px;
+  font-family: Oswald;
+  margin-bottom: 20px;
   display: flex;
   justify-content: center;
-  color: ${({ theme }) => theme.colors.RavenclawGreyLady};
+  color: ${({ theme }) => theme.colors.white};
 `;
 
 export const DescriptionApi = styled.p`
@@ -27,16 +60,15 @@ export const DescriptionApi = styled.p`
   font-weight: bold;
   font-size: 18px;
   margin-bottom: 10px;
-  color: ${({ theme }) => theme.colors.GriffindorYellow};
+  color: ${({ theme }) => theme.colors.GriffindorRedDark};
 `;
 
 export const DetailsButton = styled(Link)`
   padding: 10px 20px;
-  background: linear-gradient(to right, #d3a625, #bebebe);
-  color: ${({ theme }) => theme.colors.DementorBlack};
+  background-color: ${({ theme }) => theme.colors.DementorBlack};
+  /* color: ${({ theme }) => theme.colors.white}; */
+  color: white;
   border-radius: 5px;
-  text-decoration: none;
-  margin: 0 10px;
   text-decoration: none;
   display: inline;
   padding: 5px 5px;
--- src/services/api/auth.ts
+++ src/services/api/auth.ts
@@ -1,10 +1,10 @@
 import { setToken } from "../storage";
 
-const BASE_API_URL = 'http://localhost:8000/auth'
+const BASE_API_URL = "http://localhost:8000/auth";
 
 export const login = async (values: { email: string; password: string }) => {
-    let error = ''
-    try {
+  let error = "";
+  try {
     const response = await fetch(`${BASE_API_URL}/login`, {
       method: "POST",
       headers: {
@@ -13,23 +13,24 @@ export const login = async (values: { email: string; password: string }) => {
       body: JSON.stringify({ email: values.email, password: values.password }),
     });
     if (response.ok) {
-        const token = await response.json();
-        setToken(token);
+      const token = await response.json();
+      setToken(token);
     } else {
-        const errorData = await response.json();
-        error = errorData.error;
-      }
+      const errorData = await response.json();
+      error = errorData.error;
+    }
   } catch (error) {
-    error = (error as Error).message
+    // eslint-disable-next-line no-ex-assign
+    error = (error as Error).message;
     console.log((error as Error).message);
   }
 
-  return error
+  return error;
 };
 
 export const signup = async (values: { email: string; password: string }) => {
-    let error = ''
-    try {
+  let error = "";
+  try {
     const response = await fetch(`${BASE_API_URL}/signup`, {
       method: "POST",
       headers: {
@@ -38,16 +39,17 @@ export const signup = async (values: { email: string; password: string }) => {
       body: JSON.stringify({ email: values.email, password: values.password }),
     });
     if (response.ok) {
-        const token = await response.json();
-        setToken(token);
+      const token = await response.json();
+      setToken(token);
     } else {
-        const errorData = await response.json();
-        error = errorData.error;
-      }
+      const errorData = await response.json();
+      error = errorData.error;
+    }
   } catch (error) {
-    error = (error as Error).message
+    // eslint-disable-next-line no-ex-assign
+    error = (error as Error).message;
     console.log((error as Error).message);
   }
 
-  return error
+  return error;
 };
