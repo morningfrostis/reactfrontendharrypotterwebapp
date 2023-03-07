4b435acf496bbb0a68115f11f071e84cdb52c44d
ManuelRod <manuvski@gmail.com>
2023-03-07 14:41:58 +0100

    feat:introduction favorites painted on profile

--- src/services/api/profile.ts
+++ src/services/api/profile.ts
@@ -1,7 +1,9 @@
-import { normalizeProfile, Profile } from "../../models/profile";
+import { Favorites, normalizeFavorites, normalizeProfile, Profile } from "../../models/profile";
 import { getToken } from "../../services/storage";
 
 const BASE_API_URL = "http://localhost:8000/users/profile";
+const BASE_API_URL_FAVS = "http://localhost:8000/users/favorites";
+
 
 export const getUserInfo = async (): Promise<Profile | null> => {
   const token = getToken();
@@ -17,4 +19,21 @@ export const getUserInfo = async (): Promise<Profile | null> => {
   }
   const data = await response.json();
   return normalizeProfile(data);
+};
+
+export const getUserFavorites = async (): Promise<Favorites | null> => {
+  const token = getToken();
+  const response = await fetch(BASE_API_URL_FAVS, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "content-type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    return null;
+  }
+  const data = await response.json();
+  console.log(data, 'esto es la data')
+  return normalizeFavorites(data);
 };
